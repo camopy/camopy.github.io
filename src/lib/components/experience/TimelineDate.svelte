3e3803c5e5193ad8d3092030b4acfa6e16fa1916
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-10-27 17:18:57 -0300

    feat: :sparkles: changed timeline date to be vertical

--- src/lib/components/experience/TimelineDate.svelte
+++ src/lib/components/experience/TimelineDate.svelte
@@ -3,16 +3,28 @@
 </script>
 
 <div class="flex gap-4">
-  <p
-    class:text-pink-500={highlighted}
-    class:text-lg={highlighted}
-    class:italic={!highlighted}
-    class:text-gray-500={!highlighted}
-    class="w-40 text-right"
-  >
-    {startDate}
-    {endDate ? " - " + endDate : " - Now"}
-  </p>
+  <div class="flex flex-col justify-between">
+    <p
+      class:text-pink-500={highlighted}
+      class:text-lg={highlighted}
+      class:italic={!highlighted}
+      class:text-gray-500={!highlighted}
+      class="w-20 text-right"
+    >
+      {endDate || "Now"}
+    </p>
+
+    <p
+      class:text-pink-500={highlighted}
+      class:text-lg={highlighted}
+      class:italic={!highlighted}
+      class:text-gray-500={!highlighted}
+      class="w-20 text-right"
+    >
+      {startDate}
+    </p>
+  </div>
+
   <div class="flex flex-col items-center">
     <div
       class:bg-gray-500={!highlighted}
@@ -20,5 +32,10 @@
       class="mt-2 w-2 h-2 rounded-full"
     />
     <div class:border-pink-500={highlighted} class="border h-full" />
+    <div
+      class:bg-gray-500={!highlighted}
+      class:bg-pink-500={highlighted}
+      class="mb-2 w-2 h-2 rounded-full"
+    />
   </div>
 </div>
