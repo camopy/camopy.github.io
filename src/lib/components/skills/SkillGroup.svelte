a9f8720754ef564a26cc9756fbaea8d28ff76fc0
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-10-27 22:05:29 -0300

    feat: :sparkles: reduced gap between skills

--- src/lib/components/skills/SkillGroup.svelte
+++ src/lib/components/skills/SkillGroup.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <h3 class="text-lg font-bold bg-gray-200 px-2 text-center">{title}</h3>
-<div class="flex gap-10 flex-wrap justify-around">
+<div class="flex gap-2 flex-wrap justify-around">
   {#each skills as skill}
     <div class=" flex flex-col items-center gap-2">
       <img width="90px" height="90px" src={skill.image} alt={skill.name} />
