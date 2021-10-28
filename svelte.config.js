29d0b43e9b918304fb070dd95770f5c4d95ede9c
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-10-28 10:06:39 -0300

    build: :construction_worker: added deploy config

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
+import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
+
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
+    adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
   },
 
   preprocess: [
