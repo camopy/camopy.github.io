71d511f9ef93d984c869e1a4d33b6941479e95e9
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-10-27 22:22:01 -0300

    feat: :sparkles: added education section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import About from "$lib/components/about/About.svelte";
+  import Education from "$lib/components/education/Education.svelte";
   import Experience from "$lib/components/experience/Experience.svelte";
   import Skills from "$lib/components/skills/Skills.svelte";
 </script>
@@ -9,5 +10,6 @@
     <About />
     <Experience />
     <Skills />
+    <Education />
   </div>
 </div>
