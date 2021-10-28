259604132fe6ef9c0bee43446ae652262efcd8f9
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-10-28 11:13:24 -0300

    fix:

--- src/lib/components/menu/MenuItem.svelte
+++ src/lib/components/menu/MenuItem.svelte
@@ -8,13 +8,18 @@
   }
 </script>
 
-<a href={link} on:click={handleClick}>
-  <li
-    class:bg-gray-700={$selected === name}
-    class:border-accent={$selected === name}
-    class:border-secondary={$selected !== name}
-    class="h-12 text-primary text-lg flex justify-center items-center hover:bg-gray-600 border-l-4 hover:border-accent"
+<li
+  class:bg-gray-700={$selected === name}
+  class:border-accent={$selected === name}
+  class:border-secondary={$selected !== name}
+  class="h-12 text-primary text-lg flex justify-center items-center hover:bg-gray-600 border-l-4 hover:border-accent"
+>
+  <a
+    class="w-full h-full flex justify-center items-center"
+    aria-label={"Navigate to the " + name + " section"}
+    href={link}
+    on:click={handleClick}
   >
-    {name}
-  </li>
-</a>
+    <span>{name}</span>
+  </a>
+</li>
